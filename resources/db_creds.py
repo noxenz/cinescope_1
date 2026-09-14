0d586173ff176964dd3a53b70f192208ec9b230e
noxenz <teddiv171@gmail.com>
2026-09-14 19:35:44 +0300

    добавлен db_creds

--- resources/db_creds.py
+++ resources/db_creds.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class PostgresCreds:
-    HOST = os.getenv('DB_HOST')
-    PORT = os.getenv('DB_PORT')
-    DATABASE = os.getenv('DB_DATABASE')
-    USERNAME = os.getenv('DB_USERNAME')
-    PASSWORD = os.getenv('DB_PASSWORD')
+class MoviesDbCreds:
+    HOST = os.getenv('DB_MOVIES_HOST')
+    PORT = os.getenv('DB_MOVIES_PORT')
+    DATABASE = os.getenv('DB_MOVIES_DATABASE')
+    USERNAME = os.getenv('DB_MOVIES_USERNAME')
+    PASSWORD = os.getenv('DB_MOVIES_PASSWORD')
